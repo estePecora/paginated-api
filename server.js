dd558581c75d3e099bab6b0638b49fd978e0f4ac
estePecora <b.estefaniapecora@gmail.com>
2021-09-30 20:31:42 -0300

    middleware-pagination-function

--- server.js
+++ server.js
@@ -12,33 +12,57 @@ const users = [
     { id: 8, userName: 'user008'},
 ]
 
-app.get('/users', (req, res) => {
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
+const posts = [
+    { id: 1, post: 'post 001'},
+    { id: 2, post: 'post 002'},
+    { id: 3, post: 'post 003'},
+    { id: 4, post: 'post 004'},
+    { id: 5, post: 'post 005'},
+    { id: 6, post: 'post 006'},
+    { id: 7, post: 'post 007'},
+    { id: 8, post: 'post 008'},
+]
 
-    const startIndex = (page -1) * limit
-    const endIndex = page * limit
-
-    const results = {}
-
-    if (endIndex < users.length) {
-        results.next = {
-            page: page + 1,
-            limit: limit
-        }
-    }
-
-    if (startIndex > 0){
-        results.previous = {
-            page: page - 1,
-            limit: limit
-        }
-    }
-
-    results.results = users.slice(startIndex, endIndex)
-
-    res.json(results)
+app.get('/users', paginatedResults(users), (req, res) => {
+    res.json(res.paginatedResults)
 
 }) 
+
+app.get('/posts', paginatedResults(posts), (req, res) => {
+    res.json(res.paginatedResults)
+
+}) 
+
+function paginatedResults(model) {
+    return (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page -1) * limit
+        const endIndex = page * limit
+
+        const results = {}
+
+        if (endIndex < model.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if (startIndex > 0){
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+
+        results.results = model.slice(startIndex, endIndex)
+
+        res.paginatedResults = results
+        next()
+
+    }
+}
 
 app.listen(3000)
